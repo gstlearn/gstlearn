81d7e1ea388f8d6ae545576878bba34cdd47c459
drenard77810 <didier.renard@mines-paristech.fr>
2021-07-02 17:04:27 +0200

    Modification cosmétiques dans Spline2

--- src/Covariances/CovGCspline2.cpp
+++ src/Covariances/CovGCspline2.cpp
@@ -38,6 +38,7 @@ CovGCspline2::~CovGCspline2()
 
 double CovGCspline2::_evaluateCov(double h) const
 {
+	// Defining some constant values
   double B = 1.;
   double A = (7. - 10. * B) / 12.;
   double C = (-7. - 2. * B) / 12.;
