788da7534762eabfa2d6436ee70aa804f84fb91b
NDesassis <nicolas.desassis@gmail.com>
2021-08-10 13:49:59 +0200

    update

---
+++ src/LithoRule/PGS.cpp
@@ -0,0 +1,23 @@
+#include "LithoRule/PGS.hpp"
+
+PGS::PGS()
+{
+
+}
+
+PGS::PGS(const PGS& pgs)
+  : _rule(pgs._rule)
+{
+
+}
+
+PGS& PGS::operator=(const PGS& pgs)
+{
+  return *this;
+}
+
+PGS::~PGS()
+{
+
+}
+
---
+++ testsCur/cpp/test_SPDECur.cpp
@@ -0,0 +1,92 @@
+/******************************************************************************/
+/* COPYRIGHT ARMINES, ALL RIGHTS RESERVED                                     */
+/*                                                                            */
+/* THE CONTENT OF THIS WORK CONTAINS CONFIDENTIAL AND PROPRIETARY             */
+/* INFORMATION OF ARMINES. ANY DUPLICATION, MODIFICATION,                     */
+/* DISTRIBUTION, OR DISCLOSURE IN ANY FORM, IN WHOLE, OR IN PART, IS STRICTLY */
+/* PROHIBITED WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF ARMINES         */
+/*                                                                            */
+/* TAG_SOURCE_CG                                                              */
+/******************************************************************************/
+#include "geoslib_d.h"
+#include "geoslib_f.h"
+
+
+#define VERBOSE 0
+
+/****************************************************************************/
+/*!
+** Main Program
+**
+*****************************************************************************/
+int main(int argc, char *argv[])
+
+{
+  Db          *dbgrid;
+  Model       *model;
+  SPDE_Option  s_option;
+  const char triswitch[] = "nqQ";
+  int verbose, seed, ndim, iptr, nsimu;
+  double diag,range,param;
+  VectorInt nx = { 400, 300 };
+  VectorDouble dx = { 1., 1. };
+  VectorDouble x0 = { 0., 0. };
+  
+  /***********************/
+  /* 1 - Initializations */
+  /***********************/
+
+  dbgrid   = (Db       *) NULL;
+  model    = (Model    *) NULL;
+  verbose  = 0;
+  seed     = 31415;
+  range    = 79.8;
+  param    = 1.;
+  ndim     = 2;
+  nsimu    = 10;
+  VectorDouble sill{1.};
+  VectorDouble gext{ 2*79.8, 2*79.8 };
+
+  /* 1.b - Connect the Geoslib Library */
+
+  if (setup_license("Demonstration")) goto label_end;
+
+  /* 1.c - Setup constants */
+
+  debug_reset();
+  constant_reset();
+  acknowledge_Geoslib();
+  
+  // Create the 2-D grid output file
+
+  dbgrid = db_create_grid(0,ndim,0,LOAD_BY_COLUMN,1,nx,x0,dx);
+  if (dbgrid == (Db *) NULL) goto label_end;
+  if (db_locator_attribute_add(dbgrid,LOC_X,ndim,0,0.,
+                               &iptr)) goto label_end;
+  if (db_grid_define_coordinates(dbgrid)) goto label_end;
+  if (db_extension_diag(dbgrid,&diag)) goto label_end;
+    
+  // Model 
+
+  model = model_init(2,1,diag);
+  if (model == (Model *) NULL) goto label_end;
+  if (model_add_cova(model,COV_BESSEL_K,0,0,range,param,
+                     VectorDouble(),VectorDouble(),sill)) goto label_end;
+
+  // Perform the non-conditional simulation
+
+  s_option = spde_option_alloc();
+  spde_option_update(s_option,triswitch);
+  if (spde_f(NULL,dbgrid,model,gext,s_option,1,1,
+           seed,nsimu,0,0,0,0,0,0,0,verbose))
+    goto label_end;
+  
+  // Print statistics on the results
+
+  db_print(dbgrid,1,1,1,1);
+  
+label_end:
+  dbgrid   = db_delete(dbgrid);
+  model    = model_free(model);
+  return(0);
+}
