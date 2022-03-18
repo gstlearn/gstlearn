b6ae65c05a2e5c6524552a5ca709ba7d3b9965f0
fabien-ors <fabien.ors@mines-paristech.fr>
2022-03-18 18:14:13 +0100

    Update rgstlearn_loader and add plot.r (TO BE FIXED !!)

--- doc/dev/rgstlearn_loader.R
+++ doc/dev/rgstlearn_loader.R
@@ -1,19 +1,26 @@
-args = commandArgs(trailingOnly=TRUE)
-libdir = args[1]
-dyn.load(paste0(libdir,"rgstlearn.so"))
-cacheMetaData(1)
-rgeostats <- new.env()
-eval(parse(paste0(libdir,"rgstlearn.R")), envir=rgeostats)
-attach(rgeostats)
+# Example of libdir :
+# /home/fors/Projets/gstlearn/gstlearn/build/r/Release/gstlearn
 
-# Add default show S4 methods to all objects (assuming that they inherits from AStringable)
-cl = showMethods('$', printTo=FALSE)
-cl = cl[grep("^x=\"_p_", cl)]
-cl = gsub("^x=\"","",cl)
-cl = gsub("\"","",cl)
-invisible(lapply(cl,function(x) { 
-  cmd = paste0("setMethod(f = \"show\", signature = \"",x,"\"",
-               ", definition = function(object){ ",
-               "AStringable_display(object) })")
-  eval(parse(text=cmd))
-}))
+load_gstlearn = function(libdir)
+{
+  dyn.load(paste0(libdir,"/gstlearn.so"))
+  cacheMetaData(1)
+  rgeostats <- new.env()
+  eval(parse(paste0(libdir,"/gstlearn.R")), envir=rgeostats)
+  attach(rgeostats)
+
+  # Add default show S4 methods to all objects (assuming that they inherits from AStringable)
+  cl = showMethods('$', printTo=FALSE)
+  cl = cl[grep("^x=\"_p_", cl)]
+  cl = gsub("^x=\"","",cl)
+  cl = gsub("\"","",cl)
+  invisible(lapply(cl,function(x) { 
+    cmd = paste0("setMethod(f = \"show\", signature = \"",x,"\"",
+                 ", definition = function(object){ ",
+                 "AStringable_display(object) })")
+    eval(parse(text=cmd))
+  }))
+  
+  
+  source(paste0(libdir,"/plot.r"))
+}
