87a73ca73bf07007047cc560d61dee88ac5c0413
drenard77810 <didier.renard@minesparis.psl.eu>
2022-12-19 10:19:47 +0100

    MAJ des cours en R

--- doc/courses/r/01_gstlearn_start.R
+++ doc/courses/r/01_gstlearn_start.R
@@ -2,10 +2,9 @@
 ## Fichier test
 ##
 library(gstlearn)
+library(ggplot2)
 
 db = DbGrid_create(nx=c(100,100))
 model = Model_createFromParam(type = ECov_CUBIC(), range = 30)
-err = simtub(NULL, db, model, NULL, nbsimu=1, seed=13126, nbtuba = 1000)
-# on aurait aime faire ... mais ca ne fonctionne pas!
-# err = simtub(, db, model, nbtuba=1000)
+err = simtub(NULL, db, model, nbtuba=1000)
 plot.grid(db,name="Simu",title="Check is successful!")
