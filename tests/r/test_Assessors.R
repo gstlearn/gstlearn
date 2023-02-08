48bad7d14002c793b46518f0a8b789580bc49383
drenard77810 <didier.renard@minesparis.psl.eu>
2023-02-08 17:35:30 +0100

    J'ai fusionne les test*Brackets de python et de R en creant plutot un test_Assessors.py ou idem.R

--- tests/r/test_Assessors.R
+++ tests/r/test_Assessors.R
@@ -3,7 +3,41 @@
 suppressWarnings(suppressMessages(library(gstlearn)))
 set.seed(32421)
 
-# Creating a Table
+#
+# This part is to demonstrate the use of assessors when manipulating Db
+#
+
+a = DbGrid_create(nx=c(2,2),dx=c(1.,1.))
+nech = a$getSampleNumber()
+a$display()
+
+a["var1"] = rnorm(nech)
+a$display()
+
+print(a["var1"])
+a["var1"] = rnorm(nech)
+print(a["var1"])
+
+a["var2"] = rnorm(nech)
+a$display()
+print(a["var*"])
+
+a["var*"] = a["var*"]>0 
+print(a["var*"])
+
+a["newvar"] = c(runif(nech), rnorm(nech))
+a$display()
+print(a["newvar*"])
+
+v = a["newvar*"]
+v[1,1] = NA
+a["newvar*"] = v
+print(a["newvar*"])
+
+#
+# This part is to demonstrate the use of assessors when manipulating Table
+#
+
 table = Table(2,3)
 table$setRowNames(c("Row1","Row2"))
 table$setColumnNames(c("Col1","Col2","Col3"))
@@ -12,6 +46,10 @@ table
 newtab = table$toTL()
 class(newtab)
 newtab
+
+#
+# This part is to demonstrate the use of assessors when manipulating Matrix
+#
 
 # Creating a vector of Uniform values to fill the Rectangular Matrix
 nrow = 4
