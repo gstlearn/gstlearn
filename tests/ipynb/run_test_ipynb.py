d2fafb0d6f374e404cc413c4ea6be9d23feea5c5
fabien-ors <fabien.ors@mines-paristech.fr>
2023-02-15 16:00:48 +0100

    Hide cholesky diff in check_ipynb

--- tests/ipynb/run_test_ipynb.py
+++ tests/ipynb/run_test_ipynb.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import os
+import re
 
 # This script execute a jupyter notebook test script (argv[1]) and convert it to an asciidoc file saved in the given directory (argv[2])
 python_exe = os.path.realpath(sys.executable)
@@ -22,7 +23,7 @@ with open(test_script) as f:
 ep = ExecutePreprocessor(timeout=-1, kernel_name='python3')
 ep.preprocess(nb)
 
-# export to Asciidoc (dump only output cells)
+# export to asciidoc (dump only output cells)
 ascii_exporter = ASCIIDocExporter()
 ascii_exporter.exclude_input = True
 ascii_exporter.exclude_markdown = True
@@ -30,10 +31,13 @@ ascii_exporter.exclude_raw = True
 ascii_exporter.exclude_unknown = True
 ascii_data, resources = ascii_exporter.from_notebook_node(nb)
 
-# Remove all graphical object Identifiers form the output ascii file, i.e. :
+# Remove all graphical 3D object identifiers from the output ascii file, i.e. :
 # [[e43b6f2f-ba2b-47f7-8a13-2336077446d1]]
-import re
 ascii_data = re.sub("[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}", "XXX", ascii_data)
+
+# Remove a specific warning in Tuto_SpatioTemp.ipynb, i.e. :
+# /tmp/ipykernel_24563/4216505814.py:15: CholmodTypeConversionWarning: converting matrix of class csr_matrix to CSC format
+ascii_data = re.sub(".*CholmodTypeConversionWarning", "XXX: CholmodTypeConversionWarning", ascii_data)
 
 # write to output file
 with open(test_output, "w") as f:
