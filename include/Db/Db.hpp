fe00f2b3ee9706d74a74b4e83b1cbb12d85219ea
drenard77810 <didier.renard@minesparis.psl.eu>
2023-08-30 10:08:20 +0200

    Modif doc de ABiTargetCheck et modif de nom d'argument (ND)

--- include/Db/Db.hpp
+++ include/Db/Db.hpp
@@ -194,7 +194,7 @@ public:
                           const ELoc& locatorType = ELoc::fromKey("UNKNOWN"),
                           int locatorIndex = 0,
                           bool cleanSameLocator = false);
-  void setLocator(const String& names,
+  void setLocator(const String& name,
                   const ELoc& locatorType = ELoc::fromKey("UNKNOWN"),
                   int locatorIndex = 0,
                   bool cleanSameLocator = false);
--- include/Geometry/ABiTargetCheck.hpp
+++ include/Geometry/ABiTargetCheck.hpp
@@ -17,7 +17,15 @@
 #include "Space/SpaceTarget.hpp"
 
 class Db;
-
+/**
+ * This class performs the test between two Space Points (usually the target point first and the tentative point second)
+ * The function isOK returns TRUE if the two space points are "comparable" with respect to the current criterion
+ * Examples:
+ * - if the criterion is based on FAULTS, the method isOK returns TRUE if the two space points
+ * are not separated by any fault segment
+ * - if the criterion is based on DISTANCE, the method isOK returns TRUE if the distance between the two space points
+ * is smaller than a threshold distance (registered as a member of the BiTargetCheckDistance class)
+ */
 class GSTLEARN_EXPORT ABiTargetCheck: public AStringable//, public ICloneable
 {
 public:
