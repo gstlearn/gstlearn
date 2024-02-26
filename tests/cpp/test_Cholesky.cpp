f7d5fe155d7b00263a9023d89805e99d97a07f5f
Fabien <fabien.ors@mines-paristech.fr>
2024-02-26 12:02:52 +0100

    test anam & cholesky for Mac Os

--- tests/cpp/test_Cholesky.cpp
+++ tests/cpp/test_Cholesky.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
   double proba = 0.05;
 
   // We create a square matrix (not necessarily sparse)
-
+std::cout << "Coucou 1" << std::endl;
   NF_Triplet NF_T;
   for (int icol = 0; icol < n; icol++)
     for (int irow = 0; irow < n; irow++)
@@ -46,14 +46,14 @@ int main(int argc, char *argv[])
       NF_T.add(irow, icol, value);
     }
   MatrixSparse *A = MatrixSparse::createFromTriplet(NF_T);
-
+  std::cout << "Coucou 2" << std::endl;
   // The symmetric matrix is obtained as t(A) %*% A -> M is symmetric
 
   MatrixSparse* At = A->transpose();
   MatrixSparse* Q = MatrixFactory::prodMatMat<MatrixSparse>(A, At);
 
   // Create a vector random gaussian values
-
+  std::cout << "Coucou 3" << std::endl;
   VectorDouble vecin = VH::simulateGaussian(n);
   VectorDouble vecout1(n);
   VectorDouble vecout2(n);
@@ -67,14 +67,14 @@ int main(int argc, char *argv[])
       double value = Q->getValue(irow, icol);
       M.setValue(irow, icol, value);
     }
-
+  std::cout << "Coucou 4" << std::endl;
   // Create the Cholesky object
 
   Cholesky Qchol(Q);
   message("Matrix used to demonstrate Cholesky Algebra\n");
 
   // Checking Product
-
+  std::cout << "Coucou 5" << std::endl;
   M.prodMatVecInPlace(vecin, vecout1);
   Qchol.evalDirect(vecin, vecout2);
   if (VH::isSame(vecout1,  vecout2))
@@ -86,8 +86,9 @@ int main(int argc, char *argv[])
   }
 
   // Checking Inverse
-
+  std::cout << "Coucou 6" << std::endl;
   (void) M.solve(vecin, vecout1);
+  std::cout << "Coucou 7" << std::endl;
   Qchol.evalInverse(vecin, vecout2);
   if (VH::isSame(vecout1,  vecout2))
     message("Product Mat^{-1} %*% V is validated\n");
@@ -96,11 +97,12 @@ int main(int argc, char *argv[])
     VH::display("Product Mat^{-1} %*% V (by Matrix)", vecout1);
     VH::display("Product Mat^{-1} %*% V (by Cholesky)", vecout2);
   }
-
+  std::cout << "Coucou 8" << std::endl;
   // Checking the Estimation of the Stdev vector
 
   MatrixSquareSymmetric MP(M);
   (void) MP.invert();
+  std::cout << "Coucou 9" << std::endl;
   VectorDouble vecout1b = MP.getDiagonal();
   Qchol.stdev(vecout2);
   if (VH::isSame(vecout1b,  vecout2))
@@ -110,11 +112,12 @@ int main(int argc, char *argv[])
     VH::display("Standard Deviation (by Matrix)", vecout1b);
     VH::display("Standard Deviation (by Cholesky)", vecout2);
   }
-
+  std::cout << "Coucou 10" << std::endl;
   // Checking the calculation of Log(Det)
 
   double res1 = log(M.determinant());
   double res2 = Qchol.getLogDeterminant();
+  std::cout << "Coucou 11" << std::endl;
   if (ABS(res1 - res2) < EPSILON10)
     message("Log(Det) is validated\n");
   else
@@ -122,7 +125,7 @@ int main(int argc, char *argv[])
     message("Log(Det) (by Matrix) = %lf\n", res1);
     message("Log(Det) (by Cholesky) = %lf\n", res2);
   }
-
+  std::cout << "Coucou 12" << std::endl;
   // Free the pointers
 
   delete A;
