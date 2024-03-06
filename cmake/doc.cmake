794fe0ea301249b680eac68f3c842a22719dfa70
Fabien <fabien.ors@mines-paristech.fr>
2024-03-06 14:48:42 +0100

    Copy references to build directory

--- cmake/doc.cmake
+++ cmake/doc.cmake
@@ -1,5 +1,8 @@
 # Copy the data used by non-regression tests (in the build directory)
 file(COPY doc/data DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/doc/)
 
+# Copy the references used by demonstration and courses scripts (in the build directory)
+file(COPY doc/references DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/doc/)
+
 # Fix gstlearn.bib automatically
 configure_file(doc/gstlearn.bib.in doc/gstlearn.bib)
