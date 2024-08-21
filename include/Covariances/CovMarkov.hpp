2c9c2715d14c10d4446836c4480960478f16fc1b
Nicolas DESASSIS <nicolas.desassis@gmail.com>
2024-08-21 15:04:39 +0200

    fix Spde on sphere variance problem

--- include/Covariances/CovMarkov.hpp
+++ include/Covariances/CovMarkov.hpp
@@ -32,8 +32,9 @@ public:
   double getScadef() const override;
   bool   hasCovOnRn() const override { return false; }
   bool   hasSpectrumOnRn() const override { return true; }
+  bool   hasSpectrumOnSphere() const override { return true; }
   bool   hasMarkovCoeffs() const override { return true; }
-
+  double normalizeOnSphere(int n = 50,double scale = 1.) const override;
   double evaluateSpectrum(double freq) const override;
   VectorDouble getMarkovCoeffs() const override {return _markovCoeffs;}
   void   setMarkovCoeffs(VectorDouble coeffs) override { _markovCoeffs = coeffs;}
@@ -42,6 +43,10 @@ public:
 
 protected:
   VectorDouble _evaluateSpectrumOnSphere(int n, double scale = 1.) const override;
+
+private:
+  VectorDouble _evaluateSpectrumOnSphereWithoutNormalization(int n, double scale = 1.) const;
+
 
 private :
   VectorDouble _markovCoeffs;
