53b600eb1bad01c7bbdacaf52e33a901cf37c7cf
Nicolas DESASSIS <nicolas.desassis@gmail.com>
2024-09-20 18:47:21 +0200

    fix matern bug (K-Bessel)

--- src/Covariances/CovMatern.cpp
+++ src/Covariances/CovMatern.cpp
@@ -19,6 +19,7 @@
 #include "math.h"
 
 #define MAXTAB 100
+static bool bessel_Old_Style = false;
 
 CovMatern::CovMatern(const CovContext& ctxt)
 : ACovFunc(ECov::MATERN, ctxt)
@@ -57,26 +58,32 @@ double CovMatern::getScadef() const
 
 double CovMatern::_evaluateCov(double h) const
 {
-  static double TAB[MAXTAB];
-
-  double cov = 0.;
-  double third = getParam();
-  int nb = (int) floor(third);
-  double alpha = third - nb;
-  if (third <= 0 || nb >= MAXTAB) return (0.);
-  double coeff = (h > 0) ? pow(h / 2., third) : 1.;
-  cov = 1.;
-  if (h > 0)
+  if (bessel_Old_Style)
   {
-    if (besselk(h, alpha, nb + 1, TAB) < nb + 1) return (cov);
-    cov = 2. * coeff * TAB[nb] / exp(loggamma(third));
+    static double TAB[MAXTAB];
+    double cov = 0.;
+    double third = getParam();
+    int nb = (int) floor(third);
+    double alpha = third - nb;
+    if (third <= 0 || nb >= MAXTAB) return (0.);
+    double coeff = (h > 0) ? pow(h / 2., third) : 1.;
+    cov = 1.;
+    if (h > 0)
+    {
+      if (besselk(h, alpha, nb + 1, TAB) < nb + 1) return (cov);
+      cov = 2. * coeff * TAB[nb] / exp(loggamma(third));
+    }
+    return (cov);
   }
-  return (cov);
+
+  if (h == 0) return 1;
+  double third = getParam();
+  return 2. * pow(h / 2., third) * std::cyl_bessel_k(getParam(),h) / exp(loggamma(third));
 }
 
 String CovMatern::getFormula() const
 {
-  return "C(h)=\\frac{ \\left( \\frac{h}{a_t} \\right)^\\alpha}{2^{\\alpha-1}\\Gamma(\\alpha)}K_{-\\alpha} \\left( \\frac{h}{a_t} \\right)";
+  return "C(h)=\\frac{2^{1-\\nu}}{\\Gamma(\\nu)} h^\\nu K_{\\nu}( h )";
 }
 
 double CovMatern::evaluateSpectrum(double freq) const
