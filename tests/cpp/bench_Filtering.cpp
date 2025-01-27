26455140aa37595408be57ea9948b8381e404a75
drenard77810 <didier.renard@minesparis.psl.eu>
2025-01-27 18:25:50 +0100

    Adding 2 test for future

---
+++ tests/cpp/bench_Filtering.cpp
@@ -0,0 +1,102 @@
+/******************************************************************************/
+/*                                                                            */
+/*                            gstlearn C++ Library                            */
+/*                                                                            */
+/* Copyright (c) (2023) MINES Paris / ARMINES                                 */
+/* Authors: gstlearn Team                                                     */
+/* Website: https://gstlearn.org                                              */
+/* License: BSD 3-clause                                                      */
+/*                                                                            */
+/******************************************************************************/
+
+// This test is mean to check the Factorial Kriging Analysis
+
+#include "Basic/AStringFormat.hpp"
+#include "Basic/NamingConvention.hpp"
+#include "Covariances/CovContext.hpp"
+#include "Enum/ESpaceType.hpp"
+
+#include "Matrix/MatrixSquareSymmetric.hpp"
+#include "Space/ASpaceObject.hpp"
+#include "Db/Db.hpp"
+#include "Db/DbStringFormat.hpp"
+#include "Basic/Law.hpp"
+#include "Model/Model.hpp"
+#include "Basic/File.hpp"
+#include "Basic/OptDbg.hpp"
+#include "Basic/OptCustom.hpp"
+#include "Neigh/NeighUnique.hpp"
+#include "Neigh/NeighMoving.hpp"
+#include "Estimation/CalcKriging.hpp"
+
+int main(int argc, char* argv[])
+{
+  std::stringstream sfn;
+  sfn << gslBaseName(__FILE__) << ".out";
+  StdoutRedirect sr(sfn.str(), argc, argv);
+
+  // Global parameters
+  int ndim = 2;
+  law_set_random_seed(32131);
+  AStringFormat format;
+  defineDefaultSpace(ESpaceType::RN, ndim);
+
+  // Parameters
+  double oldstyle = 1.;
+  bool verbose    = true;
+  int nech        = 3;
+  int nvar        = 2; // Should not be modified (see Model)
+  bool flagSK     = true;
+  OptCustom::define("oldStyle", oldstyle);
+
+  // Generate the data base
+  Db* data = Db::createFillRandom(nech, ndim, nvar, 0);
+  data->setLocVariable(ELoc::Z, 1, 0, TEST);
+  DbStringFormat* dbfmt = DbStringFormat::create(FLAG_ARRAY);
+  data->display(dbfmt);
+
+  // Generate the target file
+  Db* target = Db::createFillRandom(1, ndim, 0, 0);
+  target->setCoordinate(0, 0, data->getCoordinate(0, 0));
+  target->setCoordinate(0, 1, data->getCoordinate(0, 1));
+
+  // Create the Model
+  Model* model = Model::create(CovContext(nvar, ndim));
+  MatrixSquareSymmetric* sillNug =
+    MatrixSquareSymmetric::createFromVD({1., 0.2, 0.2, 3.});
+  model->addCovFromParam(ECov::NUGGET, 0., 0., 0., VectorDouble(), *sillNug);
+  MatrixSquareSymmetric* sillExp =
+    MatrixSquareSymmetric::createFromVD({2., 0.1, 0.1, 1.});
+  model->addCovFromParam(ECov::EXPONENTIAL, 0.7, 0., 0., VectorDouble(), *sillExp);
+  model->setCovaFiltered(0, true);
+  if (flagSK)
+  {
+    VectorDouble means = VH::simulateGaussian(nvar);
+    model->setMeans(means);
+  }
+  else
+    model->setDriftIRF(0, 0);
+  VectorDouble means(nvar, 0.);
+
+  // Neighborhood
+  ANeigh* neigh = NeighUnique::create();
+
+  // Define the verbose option
+  if (verbose) OptDbg::setReference(1);
+
+  // Test on Collocated CoKriging in Unique Neighborhood
+  kriging(data, target, model, neigh);
+  dbfmt = DbStringFormat::create(FLAG_STATS, {"Kriging.*"});
+  target->display(dbfmt);
+
+  // Free pointers
+
+  delete sillNug;
+  delete sillExp;
+  delete neigh;
+  delete data;
+  delete target;
+  delete model;
+
+  return (0);
+}
--- tests/cpp/bench_SimtubCD.cpp
+++ tests/cpp/bench_SimtubCD.cpp
@@ -19,8 +19,10 @@
 #include "Basic/AStringable.hpp"
 #include "Basic/File.hpp"
 #include "Basic/Timer.hpp"
+#include "Basic/OptCustom.hpp"
 #include "Db/Db.hpp"
 #include "Db/DbStringFormat.hpp"
+#include "Neigh/NeighUnique.hpp"
 #include "Simulation/CalcSimuTurningBands.hpp"
 
 /****************************************************************************/
@@ -38,36 +40,46 @@ int main(int argc, char *argv[])
 
   int ndim = 2;
   defineDefaultSpace(ESpaceType::RN, ndim);
-  bool verbose = false;
+
+  // Parameters
+  int ndat        = 4;
+  int nvar        = 2;
+  int nbsimu      = 2;
+  int nbtuba      = 1000;
+  double oldstyle = 1.;
+  bool flagSK     = false;
+  OptCustom::define("oldStyle", oldstyle);
 
   // Creating the data base
-  int ndat = 4;
-  int nvar = 1;
   Db* data = Db::createFillRandom(ndat, ndim, nvar);
   
   // Creating a grid covering the same space
-  VectorInt nx = { 200, 200 };
-  VectorDouble dx = { 0.005, 0.005 };
+  VectorInt nx = { 20, 20 };
+  VectorDouble dx = { 0.05, 0.05 };
   DbGrid* grid = DbGrid::create(nx, dx);
 
   // Creating a Model(s) for simulating a variable
-  Model* model = Model::createFromParam(ECov::SPHERICAL,0.2);
+  double scale = 0.7;
+  MatrixSquareSymmetric* sills =
+    MatrixSquareSymmetric::createRandomDefinitePositive(nvar);
+  Model* model = Model::createFromParam(ECov::EXPONENTIAL, scale, 0., 0., VectorDouble(),
+                                        *sills, VectorDouble(), nullptr, false);
+  if (flagSK)
+    model->setMeans(VH::simulateGaussian(nvar));
+  else
+    model->setDriftIRF(0, 0);
 
-  // Perform a non-conditional simulation on the Grid
-  int nbsimu = 2;
-  int nbtuba = 1000;
+  // Creating the Neighborhood
+  ANeigh* neigh = NeighUnique::create();
 
-  if (verbose)
-  {
-    message("Conditional simulation(s) on grid using Turning Bands:\n");
-    message("- Data (%d) samples\n", data->getNSample());
-    message("- Grid (%d x %d)\n", grid->getNX(0), grid->getNX(1));
-    message("- Number of Bands = %d\n", nbtuba);
-    message("- Number of simulations = %d\n", nbsimu);
-  }
+  message("Conditional simulation(s) on grid using Turning Bands:\n");
+  message("- Data (%d) samples\n", data->getNSample());
+  message("- Grid (%d x %d)\n", grid->getNX(0), grid->getNX(1));
+  message("- Number of Bands = %d\n", nbtuba);
+  message("- Number of simulations = %d\n", nbsimu);
 
   timer.reset();
-  (void) simtub(data,grid,model,nullptr, nbsimu, 113423, nbtuba);
+  (void)simtub(data, grid, model, neigh, nbsimu, 113423, nbtuba);
   timer.displayIntervalMilliseconds("Non-conditional Simulation on Grid", 6800);
 
   // Produce some statistics for comparison
@@ -78,6 +90,7 @@ int main(int argc, char *argv[])
   delete data;
   delete grid;
   delete model;
+  delete neigh;
 
   return (0);
 }
