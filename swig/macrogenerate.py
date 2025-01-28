4c8f0e3d25b325514d7619be622f2507fdb005a9
Nicolas DESASSIS <nicolas.desassis@gmail.com>
2025-01-28 23:09:29 +0100

    list all files

--- swig/macrogenerate.py
+++ swig/macrogenerate.py
@@ -22,12 +22,17 @@ def extract_macro_calls(header_file , macro_name):
                         print("error.")  
                 macro_calls+= [(classname, func_name.strip(), body.strip(),arg)]
     return macro_calls
-def generate_python_code(macro_calls, output_file):
+def generate_python_code(macro_calls, output_file,first = True):
     """
     Génère du code Python pour les macros extraites.
     """
-    
-    with open(output_file, 'w') as file:
+    if len(macro_calls) == 0:
+        return first
+    if first:
+        opt = "w"
+    else:
+        opt = "a"
+    with open(output_file, opt) as file:
         file.write('%pythoncode %{\n')
         file.write(f"import gstlearn as gl\n")
         for classname, func_name, body, arg in macro_calls:
@@ -39,10 +44,17 @@ def generate_python_code(macro_calls, output_file):
             file.write("\n")
             file.write(f"setattr(gl.{classname},'{func_name}',{func_name})\n")
         file.write('%}\n')
+    return False
 
-def generate_r_code(macro_calls, output_file):
-
-    with open(output_file, 'w') as file:
+def generate_r_code(macro_calls, output_file,first = True):
+    
+    if len(macro_calls) == 0:
+        return first
+    if first:
+        opt = "w"
+    else:
+        opt = "a"
+    with open(output_file, opt) as file:
         file.write("%insert(s)%{\n")
 
         for classname, func_name, body, arg in macro_calls:
@@ -55,30 +67,43 @@ def generate_r_code(macro_calls, output_file):
             file.write(f"assign('{classname}_evalCovMatSym', f , envir = asNamespace('gstlearn'))\n")
         
         file.write("%}\n")
-
+    return False
      
-def build_macro_python(macro_name, filename,output_file = "../../python/generated_python.i"):
+def build_macro_python(macro_name, filename,output_file, first = True):
   header_file = "../../include/"+ filename
+  output_file = "../../python/" + output_file
   macro_calls = extract_macro_calls(header_file, macro_name)
-  generate_python_code(macro_calls, output_file)
+  first = generate_python_code(macro_calls, output_file, first)
   print(f"Python additional wrappers generated in {output_file}")
+  return first
 
-def build_macro_r(macro_name, filename,output_file = "../../r/generated_r.i"):
+def build_macro_r(macro_name, filename,output_file,first = True):
   header_file = "../../include/"+ filename
+  output_file = "../../r/" + output_file
   macro_calls = extract_macro_calls(header_file, macro_name)
-  print(macro_calls)
-  generate_r_code(macro_calls, output_file)
+  first = generate_r_code(macro_calls, output_file, first)
   print(f"R additional wrappers generated in {output_file}")
+  return first
 
 macro_name = "FORWARD_METHOD"  # Nom de la macro à analyser
 header_file = "Model/ModelGeneric.hpp"  # Fichier d'en-tête à analyser
+output_file = "generated.i"  # Fichier de sortie
+includedir = "../../include/"
 
-print(os.getcwd())
-if sys.argv[1] == "python":
-    build_macro_python(macro_name,header_file)
-if sys.argv[1] == "r":
-    build_macro_r(macro_name,header_file)
-
+tree = os.walk(os.path.join(os.pardir,os.pardir,"include"))
+first = True
+for dir,root,files in tree:
+    path = os.path.split(dir)[1]
+    if path in ["include","Core"]:
+        continue
+    for file in files:
+        if file.split(".")[-1] != "hpp":
+            continue
+        header_file = os.path.join(path,file)
+        if sys.argv[1] == "python":
+            first = build_macro_python(macro_name,header_file,"generated_python.i",first)
+        if sys.argv[1] == "r":
+            first = build_macro_r(macro_name,header_file,"generated_r.i",first)
 
 
 
