ad0997a8d54dfa5ec549053974ddf728bdc966fa
Fabien Ors <fabien.ors@minesparis.psl.eu>
2025-02-13 11:16:02 +0100

    Really fix newobject for all create* methods

--- swig/classanalysis.py
+++ swig/classanalysis.py
@@ -5,7 +5,8 @@ import os
 
 
 def process_cpp_file(classname,header_file, output_file, first):
-    pattern = re.compile(r'\w+\s*\*\s*(create[a-zA-Z0-9_]*)')
+    # Catch all functions named 'createFrom*' or 'createSomething*'
+    pattern = re.compile(r'\w+\s*\*\s*(create\w+)')
     if first:
         mode = 'w'
     else:
