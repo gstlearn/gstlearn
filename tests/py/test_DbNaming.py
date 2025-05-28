909e84b095a9c7f25b7d7b9ab78437876695a0ea
Pierre Guillou <pierre.guillou@minesparis.psl.eu>
2025-05-28 15:00:23 +0200

    [tests/py] Remove ASerializable::setContainerName calls

--- tests/py/test_DbNaming.py
+++ tests/py/test_DbNaming.py
@@ -18,7 +18,6 @@ gl.mestitle(0,"Preparing the Environment")
 
 ndim = 2
 gl.defineDefaultSpace(gl.ESpaceType.RN,ndim)
-gl.ASerializable.setContainerName(True)
 gl.ASerializable.setPrefixName("DbTest-");
 
 # The following object will enable having a complete view of
