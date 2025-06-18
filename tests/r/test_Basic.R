9324ee63c08e5e51b5eaa10406cff87338e7db6b
Fabien Ors <fabien.ors@minesparis.psl.eu>
2025-06-18 16:51:32 +0200

    Add display in test_Basic

--- tests/r/test_Basic.R
+++ tests/r/test_Basic.R
@@ -4,24 +4,26 @@ suppressWarnings(suppressMessages(library(gstlearn)))
 
 mygrid = DbGrid_create(nx=c(100,150), dx=c(1.,1.))
 
-#We create a geostatistical Model constituted of a single Spherical anisotropic structure with a sill of 1 and a shortest range of 30m and a longest one of 50m. The orientation of the long range is in the direction 30 degrees from East counted counter-clockwise.
+# We create a geostatistical Model constituted of a single Spherical anisotropic structure with a sill of 1 and a shortest range of 30m and a longest one of 50m. The orientation of the long range is in the direction 30 degrees from East counted counter-clockwise.
 
-mymodel = Model_createFromParam(ECov_SPHERICAL(), sill=1, ranges=c(50,30), angles=c(30,0))
+mymodel = Model_createFromParam(ECov_SPHERICAL(), sill = 1, ranges = c(50, 30), angles = c(30, 0))
+mymodel$display()
 
 #We perform one non-conditional simulation using the Turning Band method (the number of bands is set to 1000). This results in adding a field (commonly referred to as a variable) in mygrid which is called Data. Typing the name of the grid data base is an easy way to get a summary of its contents.
 
 err = simtub(NULL, mygrid, mymodel, nbtuba=1000, namconv=NamingConvention("Data"))
-mygrid
+mygrid$display()
 
 # We sample the grid on a set of 100 samples randomly located within the area covered by the grid. Typing the name of the newly created data base (mypoints) give the list of variables already stored in this data base.
 
 mypoints = Db_createSamplingDb(mygrid, number=100)
-mypoints
+mypoints$display()
 
 #We calculate a variogram along the two main directions of the Model, i.e. 30 and 120 degrees.
 
 varioparam = VarioParam_createSeveral2D(angles=c(30,120), nlag=25, dlag=2)
 myvario = Vario_computeFromDb(varioparam, mypoints)
+myvario$display()
 
 #In the next step, we consider that the initial model (mymodel) has been correctly rendered through the simulated information at the 100 samples, as demonstrated in the experimental variogram. This is the reason why we will use this initial model for the further steps of estimation and conditional simulations.
 
@@ -35,8 +37,9 @@ myneigh = NeighUnique_create()
 #- the one corresponding to the standard deviation of the estimation error (called kriging.Simu.stdev)
 
 err = kriging(dbin=mypoints, dbout=mygrid, model=mymodel, neigh=myneigh)
-mygrid
+mygrid$display()
 
-#We can now construct 2 conditional simulations, using the Turning Bands algorithm again, but conditioned by the variable informed at the samples of mypoints. The newly created variables in mygrid are called *Simu.Data.1" and "Simu.Data.2". Each simulation outcome reproduces the spatial characteristics provided by the Model and honors the information provided at sample points.
+# We can now construct 2 conditional simulations, using the Turning Bands algorithm again, but conditioned by the variable informed at the samples of mypoints. The newly created variables in mygrid are called *Simu.Data.1" and "Simu.Data.2". Each simulation outcome reproduces the spatial characteristics provided by the Model and honors the information provided at sample points.
 
-err = simtub(dbin=mypoints, dbout=mygrid, model=mymodel, neigh=myneigh, nbtuba=1000, nbsimu=2)
+err = simtub(dbin = mypoints, dbout = mygrid, model = mymodel, neigh = myneigh, nbtuba = 1000, nbsimu = 2)
+mygrid$display()
