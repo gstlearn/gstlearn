df03792a4d0b37d668d8727f43316002521fbdb1
Xavier Freulon <xavier.freulon@minesparis.psl.eu>
2025-07-08 15:33:32 +0200

    Prepare R test

--- tests/r/test_Matern.R
+++ tests/r/test_Matern.R
@@ -88,7 +88,8 @@ for (ivar in 1:nvar) {
   }
 }
 
-model = Model()
+ctxt = CovContext(nvar = 3)
+model = ModelGeneric(ctxt)
 model$setCov(cor_tri)
 grid = DbGrid_create(c(100,100))
 simuSpectral(, dbout = grid, model = model, nbsimu = 1, seed = 43431, ns = 100, nd = 100, 
