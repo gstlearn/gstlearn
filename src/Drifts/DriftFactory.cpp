fa6929861fac9e6540fc7494b15812c338685ebd
drenard77810 <didier.renard@minesparis.psl.eu>
2025-09-30 15:35:34 +0200

    Correcting the error for MAC

--- src/Drifts/DriftFactory.cpp
+++ src/Drifts/DriftFactory.cpp
@@ -159,7 +159,7 @@ DriftList* DriftFactory::createDriftListFromIRF(Id order,
                                                 const CovContext& ctxt)
 {
   auto* drifts = new DriftList(ctxt);
-  auto ndim    = ctxt.getNDim();
+  Id ndim      = ctxt.getNDim();
 
   // In the strict stationary case, no drift is defined (even external)
   if (order < 0)
