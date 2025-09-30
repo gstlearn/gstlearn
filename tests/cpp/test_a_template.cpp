9b115fc6b4054fc7643f7e012e9161dd91e4a5a0
drenard77810 <didier.renard@minesparis.psl.eu>
2025-09-30 15:32:57 +0200

    Start debugging Vecchia

--- tests/cpp/test_a_template.cpp
+++ tests/cpp/test_a_template.cpp
@@ -8,22 +8,11 @@
 /* License: BSD 3-clause                                                      */
 /*                                                                            */
 /******************************************************************************/
-/**
- * This file is meant to parametrized the ModelGeneric in terms of ParamInfo
- * and to fit the values of these parameters according to the Maximum LogLikelihood
- * method and using the Vecchia approximation.
- */
-#include "Basic/OptDbg.hpp"
-#include "Db/DbGrid.hpp"
+#include "Basic/VectorT.hpp"
+#include "Db/Db.hpp"
 #include "Db/DbStringFormat.hpp"
-#include "Enum/ESpaceType.hpp"
-#include "Estimation/CalcGlobal.hpp"
-#include "Estimation/CalcKriging.hpp"
+#include "Estimation/Vecchia.hpp"
 #include "Model/Model.hpp"
-#include "Neigh/NeighUnique.hpp"
-#include "Space/ASpaceObject.hpp"
-#include "Variogram/DirParam.hpp"
-#include "Variogram/Vario.hpp"
 #include "geoslib_define.h"
 
 using namespace gstlrn;
@@ -33,74 +22,41 @@ int main(int argc, char* argv[])
   DECLARE_UNUSED(argc);
   DECLARE_UNUSED(argv);
 
-  defineDefaultSpace(ESpaceType::RN, 2);
+  std::stringstream sfn;
+  sfn << gslBaseName(__FILE__) << ".out";
+  StdoutRedirect sr(sfn.str(), argc, argv);
+  ASerializable::setPrefixName("test_a_template-");
 
-  String filename = "/home/drenard/Rtest/Bez/myDb.NF";
-  bool hasDrift   = true;
-  bool flagLocal  = false;
-  double propSel  = 0.1;
-  Id optDebug     = 0;
+  auto* model = Model::createFromParam(ECov::EXPONENTIAL);
+  model->setDriftIRF(0);
 
-  Db* myDb = Db::createFromNF(filename);
-  myDb->setLocators({"longitude", "latitude"}, ELoc::X);
-  myDb->setLocators({"density", "depth"}, ELoc::Z);
-  myDb->deleteColumnsByLocator(ELoc::SEL);
-  if (propSel > 0)
-    myDb->addSelectionRandom(propSel);
-  else
-    myDb->clearSelection();
+  Id nbvecchia = 4;
+  double like;
+  bool verbose = true;
 
-  DbStringFormat* dbfmt = DbStringFormat::createFromFlags(false, false, false, true, true);
-  myDb->display(dbfmt);
-  message("Number of active samples = %d\n", myDb->getNSampleActive());
+  Id nvar  = 1;
+  Id dim   = 2;
+  Id ndat  = 6;
+  Id indNa = 3;
+  Db* db   = Db::createFillRandom(ndat, dim, nvar, 0, 0, 0);
+  db->setValue("z", indNa, TEST);
+  auto dbfmt = DbStringFormat(FLAG_ARRAY, VectorString(), VectorInt(), false);
+  db->display(&dbfmt);
 
-  DbGrid* myGrid = DbGrid::createCoveringDb(myDb, VectorInt(), {10, 10});
-  message("Number of grid nodes = %d\n", myGrid->getNSample());
+  like = logLikelihoodVecchia(db, model, nbvecchia, verbose);
+  message("Likelihood without filtering %lf\n", like);
 
-  NeighUnique myNeighU = NeighUnique();
+  VectorDouble sel(ndat, 1.);
+  sel[indNa] = 0.;
+  db->addSelection(sel, "sel");
+  db->display(&dbfmt);
+  like = logLikelihoodVecchia(db, model, nbvecchia, verbose);
+  message("Likelihood with selection %lf\n", like);
 
-  VarioParam* varioparam = VarioParam::createOmniDirection(10, 50);
-  Vario* myVarioOmni     = Vario::computeFromDb(*varioparam, myDb);
+  db->deleteSample(indNa);
+  db->display(&dbfmt);
+  like = logLikelihoodVecchia(db, model, nbvecchia, verbose);
+  message("Likelihood after deleting the NA sample %lf\n", like);
 
-  Model myModelBi;
-  myModelBi.fit(myVarioOmni, {ECov::NUGGET, ECov::EXPONENTIAL, ECov::SPHERICAL});
-  if (hasDrift) myModelBi.setDriftIRF(0, 0);
-
-  OptDbg::setReference(optDebug);
-  Id mode = 0;
-
-  if (mode == 0 || mode == 2)
-  {
-    // CoKriging
-    myDb->setLocators({"density", "depth"}, ELoc::Z);
-    if (flagLocal)
-    {
-      (void)kriging(myDb, myGrid, &myModelBi, &myNeighU, true, true, false, KrigOpt(),
-                    NamingConvention("CoK"));
-    }
-    else
-    {
-      Global_Result resBivariate = global_kriging(myDb, myGrid, &myModelBi);
-      resBivariate.display();
-    }
-  }
-
-  if (mode == 0 || mode == 1)
-  {
-    // Kriging of first variable
-
-    myDb->setLocator("density", ELoc::Z, 0);
-    Model* myModelUni = myModelBi.createReduce({0});
-    if (flagLocal)
-    {
-      (void)kriging(myDb, myGrid, myModelUni, &myNeighU, true, true, false, KrigOpt(),
-                    NamingConvention("K"));
-    }
-    else
-    {
-      Global_Result resUnivariate = global_kriging(myDb, myGrid, myModelUni);
-      resUnivariate.display();
-    }
-    delete myModelUni;
-  }
+  return 0;
 }
