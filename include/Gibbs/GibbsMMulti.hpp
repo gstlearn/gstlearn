e5ece031c6dc2f09c1ac84a6148edda9351ac0ec
drenard77810 <didier.renard@minesparis.psl.eu>
2025-10-06 10:03:52 +0200

    Revisite Gibbs_sampler

--- include/Gibbs/GibbsMMulti.hpp
+++ include/Gibbs/GibbsMMulti.hpp
@@ -15,7 +15,6 @@
 #include "Gibbs/GibbsMulti.hpp"
 #include "LinearOp/CholeskySparse.hpp"
 
-
 namespace gstlrn
 {
 class Db;
@@ -27,45 +26,45 @@ class GSTLEARN_EXPORT GibbsMMulti: public GibbsMulti
 public:
   GibbsMMulti();
   GibbsMMulti(Db* db, Model* model);
-  GibbsMMulti(const GibbsMMulti &r);
-  GibbsMMulti& operator=(const GibbsMMulti &r);
+  GibbsMMulti(const GibbsMMulti& r);
+  GibbsMMulti& operator=(const GibbsMMulti& r);
   virtual ~GibbsMMulti();
 
-  void update(VectorVectorDouble &y, Id isimu, Id ipgs, Id iter) override;
+  void update(VectorVectorDouble& y, Id isimu, Id ipgs, Id iter) override;
   Id covmatAlloc(bool verbose, bool verboseTimer = false) override;
 
   void setEps(double eps) { _eps = eps; }
   void cleanup() override;
 
-  void setFlagStoreInternal(bool flagStoreInternal) ;
+  void setFlagStoreInternal(bool flagStoreInternal);
 
 private:
-  Id  _getNVar() const;
-  Id  _getSize() const;
+  Id _getNVar() const;
+  Id _getSize() const;
   void _storeWeights(Id icol);
   void _storeWeightsMS(Id icol, NF_Triplet& NF_T);
   void _getWeights(Id icol) const;
   void _calculateWeights(Id icol,
-                         VectorDouble &b,
+                         VectorDouble& b,
                          double tol = EPSILON3) const;
-  Id  _storeAllWeights(bool verbose = false);
-  Id  _getSizeOfWeights(const VectorDouble& weights) const;
-  double _getEstimate(Id ipgs, Id icol, const VectorVectorDouble &y) const;
+  Id _storeAllWeights(bool verbose = false);
+  Id _getSizeOfWeights(const VectorDouble& weights) const;
+  double _getEstimate(Id ipgs, Id icol, const VectorVectorDouble& y) const;
   void _allocate();
   double _getVariance(Id icol) const;
   Id _getColumn(Id iact, Id ivar) const;
-  void _splitCol(Id icol, Id *iact, Id *ivar) const;
+  void _splitCol(Id icol, Id* iact, Id* ivar) const;
   void _updateStatWeights(Id* nzero);
 
 private:
-  std::shared_ptr<MatrixSparse>   _Cmat;
+  std::shared_ptr<MatrixSparse> _Cmat;
   CholeskySparse* _CmatChol;
-  double          _eps;
-  bool            _flagStoreInternal;
+  double _eps;
+  bool _flagStoreInternal;
 
   VectorVectorDouble _areas;
-  MatrixSparse*      _matWgt;
+  MatrixSparse* _matWgt;
 
   mutable VectorDouble _weights;
 };
-}
+} // namespace gstlrn
--- include/Gibbs/GibbsMultiMono.hpp
+++ include/Gibbs/GibbsMultiMono.hpp
@@ -10,21 +10,21 @@
 /******************************************************************************/
 #pragma once
 
-#include "gstlearn_export.hpp"
 #include "Gibbs/AGibbs.hpp"
+#include "gstlearn_export.hpp"
 
 namespace gstlrn
 {
 class Db;
 class Model;
 
-class GSTLEARN_EXPORT GibbsMultiMono : public AGibbs
+class GSTLEARN_EXPORT GibbsMultiMono: public AGibbs
 {
 public:
   GibbsMultiMono();
   GibbsMultiMono(Db* db, const std::vector<Model*>& models, double rho);
-  GibbsMultiMono(const GibbsMultiMono &r);
-  GibbsMultiMono& operator=(const GibbsMultiMono &r);
+  GibbsMultiMono(const GibbsMultiMono& r);
+  GibbsMultiMono& operator=(const GibbsMultiMono& r);
   virtual ~GibbsMultiMono();
 
   Model* getModels(Id ivar) const { return _models[ivar]; } // TODO: protect by const asap
@@ -32,7 +32,7 @@ public:
   Id getNVar() const { return static_cast<Id>(_models.size()); }
 
   /// Interface for AGibbs
-  Id calculInitialize(VectorVectorDouble &y, Id isimu, Id ipgs) override;
+  Id calculInitialize(VectorVectorDouble& y, Id isimu, Id ipgs) override;
   double getSimulate(VectorVectorDouble& y,
                      double yk,
                      double sk,
@@ -44,7 +44,7 @@ public:
   Id checkGibbs(const VectorVectorDouble& y, Id isimu, Id ipgs) override;
 
 private:
-  std::vector<Model *> _models;
+  std::vector<Model*> _models;
   double _rho;
 };
-}
+} // namespace gstlrn
