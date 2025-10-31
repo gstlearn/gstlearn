d90970b4739683ef6e596adabae6f89fcd72aabf
drenard77810 <didier.renard@minesparis.psl.eu>
2025-10-31 18:13:17 +0100

    Correction of Issue 751

--- tests/cpp/test_Vario.cpp
+++ tests/cpp/test_Vario.cpp
@@ -16,15 +16,15 @@
 #include "Enum/ECalcVario.hpp"
 #include "Enum/ECov.hpp"
 
-#include "Variogram/Vario.hpp"
-#include "Variogram/VMap.hpp"
-#include "Model/Model.hpp"
 #include "Basic/AStringable.hpp"
 #include "Basic/File.hpp"
+#include "Covariances/CovAniso.hpp"
 #include "Db/Db.hpp"
 #include "Db/DbStringFormat.hpp"
-#include "Covariances/CovAniso.hpp"
+#include "Model/Model.hpp"
 #include "Simulation/CalcSimuTurningBands.hpp"
+#include "Variogram/VMap.hpp"
+#include "Variogram/Vario.hpp"
 
 using namespace gstlrn;
 
@@ -33,7 +33,7 @@ using namespace gstlrn;
 ** Main Program for testing the sparse matrix algebra
 **
 *****************************************************************************/
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   std::stringstream sfn;
   sfn << gslBaseName(__FILE__) << ".out";
@@ -42,32 +42,32 @@ int main(int argc, char *argv[])
   Id error = 1;
   Id ndim  = 2;
   defineDefaultSpace(ESpaceType::RN, ndim);
-  CovContext ctxt(1,2,1.); // use default space
+  CovContext ctxt(1, 2, 1.); // use default space
 
   // Creating a Point Data base in the 1x1 square with 'nech' samples
   Id nech = 1000;
-  Db* db = Db::createFromBox(nech,{0.,0.},{1.,1.}, 3242);
+  Db* db  = Db::createFromBox(nech, {0., 0.}, {1., 1.}, 3242);
   db->display();
 
   // Creating a grid covering the same space
-  VectorInt nx = { 100, 100 };
-  VectorDouble dx = { 0.01, 0.01 };
-  DbGrid* grid = DbGrid::create(nx, dx);
+  VectorInt nx    = {100, 100};
+  VectorDouble dx = {0.01, 0.01};
+  DbGrid* grid    = DbGrid::create(nx, dx);
   grid->display();
 
   // Creating the Model(s) of the Underlying GRF(s)
   Model models(ctxt);
   CovAnisoList covs(ctxt);
   double range1 = 0.2;
-  CovAniso cova1(ECov::MATERN,range1,1.,1.,ctxt);
+  CovAniso cova1(ECov::MATERN, range1, 1., 1., ctxt);
   covs.addCov(cova1);
   models.setCovAnisoList(&covs);
   models.display();
 
   // Perform a non-conditional simulation on the Db and on the Grid
-  error = simtub(nullptr,db,&models);
+  error = simtub(nullptr, db, &models);
   db->display();
-  error = simtub(nullptr,grid,&models);
+  error = simtub(nullptr, grid, &models);
   grid->display();
 
   // ===============
@@ -77,14 +77,14 @@ int main(int argc, char *argv[])
   mestitle(1, "Experimental variogram on Data Samples");
   Id nlag                 = 20;
   VarioParam* varioparamP = VarioParam::createMultiple(2, nlag, 0.5 / nlag);
-  Vario* variop = Vario::computeFromDb(*varioparamP,db,ECalcVario::VARIOGRAM);
+  Vario* variop           = Vario::computeFromDb(*varioparamP, db, ECalcVario::VARIOGRAM);
   variop->display();
-  message("Maximum Variogram Value = %lf\n",variop->getGmax());
+  message("Maximum Variogram Value = %lf\n", variop->getGmax());
 
   // Fitting the experimental variogram of Underlying GRF (with constraint that total sill is 1)
   Model model(ctxt);
   VectorECov covas {ECov::MATERN, ECov::EXPONENTIAL};
-  model.fit(variop,covas,false);
+  model.fit(variop, covas, false);
   model.display();
 
   // ===============
@@ -93,7 +93,7 @@ int main(int argc, char *argv[])
 
   mestitle(1, "Experimental variogram on Grid");
   VarioParam* varioparamG = VarioParam::createMultipleFromGrid(grid, nlag);
-  Vario* variog = Vario::computeFromDb(*varioparamG, grid, ECalcVario::VARIOGRAM);
+  Vario* variog           = Vario::computeFromDb(*varioparamG, grid, ECalcVario::VARIOGRAM);
   variog->display();
 
   // ==========================================
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
 
   mestitle(1, "Variogram Map on Grid");
   Db* vmapG = db_vmap(grid, ECalcVario::VARIOGRAM);
-  DbStringFormat dbfmt(FLAG_STATS,{"VMAP*"});
+  DbStringFormat dbfmt(FLAG_STATS, {"VMAP*"});
   vmapG->display(&dbfmt);
 
   delete db;
@@ -120,6 +120,46 @@ int main(int argc, char *argv[])
   delete variog;
   delete vmapG;
   delete vmapP;
+
+  // ===================================
+  // Manipulation of variogram assessors
+  // ===================================
+
+  // Create a 2-D data set and calculate an omni-directional variogram
+  auto* dat        = Db::createFillRandom(10, 2, 2);
+  auto* varioParam = VarioParam::createOmniDirection(10, 0.1);
+  auto* vario      = Vario::computeFromDb(*varioParam, dat);
+  vario->display();
+
+  bool compress = true;
+
+  // Extract the values from a multivariate variogram
+  VectorDouble vec00 = vario->getGgVec(0, 0, 0, false, false, compress);
+  VH::dump("Variogram for: ivar=0, jvar=0 (no compress)", vec00);
+
+  VectorDouble vec10 = vario->getGgVec(0, 1, 0, false, false, compress);
+  VH::dump("Variogram for: ivar=1, jvar=0 (no compress)", vec10);
+
+  VectorDouble vec11 = vario->getGgVec(0, 1, 1, false, false, compress);
+  VH::dump("Variogram for: ivar=1, jvar=1 (no compress)", vec11);
+
+  (void)vario->setGgVec(0, 0, 1, vec00);
+  (void)vario->setGgVec(0, 1, 1, vec00);
+
+  compress = false;
+
+  vec00 = vario->getGgVec(0, 0, 0, false, false, compress);
+  VH::dump("Variogram for: ivar=0, jvar=0", vec00);
+
+  vec10 = vario->getGgVec(0, 1, 0, false, false, compress);
+  VH::dump("Variogram for: ivar=1, jvar=0", vec10);
+
+  vec11 = vario->getGgVec(0, 1, 1, false, false, compress);
+  VH::dump("Variogram for: ivar=1, jvar=1", vec11);
+
+  (void)vario->setGgVec(0, 0, 1, vec00);
+  (void)vario->setGgVec(0, 1, 1, vec00);
+  vario->display();
 
   return static_cast<int>(error);
 }
