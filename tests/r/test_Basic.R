5c69da06d085e49582bc3de15e0dc042a2702887
Fabien Ors <fabien.ors@minesparis.psl.eu>
2025-11-03 10:04:34 +0100

    Fix non-reg (no more image generation in test_Basic)

--- tests/r/test_Basic.R
+++ tests/r/test_Basic.R
@@ -17,11 +17,11 @@ err = simtub(NULL, mygrid, mymodel, nbtuba=nbtuba, namconv=NamingConvention("Dat
 mygrid$display()
 
 ########################
-p = plot.init(asp=1)
-p = p + plot.raster(mygrid, name="Data", flagLegend=TRUE, legendName="Simu", limits=c(-3.5, 3.5))
-p = p + plot.decoration(title="Initial Simulated field", xlab="Easting", ylab="Northing")
-ggsave("Figure1_r.png", p, dpi=600)
-plot.end(p)
+#p = plot.init(asp=1)
+#p = p + plot.raster(mygrid, name="Data", flagLegend=TRUE, legendName="Simu", limits=c(-3.5, 3.5))
+#p = p + plot.decoration(title="Initial Simulated field", xlab="Easting", ylab="Northing")
+#ggsave("Figure1_r.png", p, dpi=600)
+#plot.end(p)
 ########################
 
 # We sample the grid on a set of 100 samples randomly located within the area covered by the grid. Typing the name of the newly created data base (mypoints) give the list of variables already stored in this data base.
@@ -36,11 +36,11 @@ myvario = Vario_computeFromDb(varioparam, mypoints)
 myvario$display()
 
 ########################
-p = plot.init()
-p = p + plot.vario(myvario, model=mymodel, flagLegend=TRUE)
-p = p + plot.decoration(title="Variogram in 2 main directions")
-ggsave("Figure2_r.png", p, dpi=600)
-plot.end(p)
+#p = plot.init()
+#p = p + plot.vario(myvario, model=mymodel, flagLegend=TRUE)
+#p = p + plot.decoration(title="Variogram in 2 main directions")
+#ggsave("Figure2_r.png", p, dpi=600)
+#plot.end(p)
 ########################
 
 #In the next step, we consider that the initial model (mymodel) has been correctly rendered through the simulated information at the 100 samples, as demonstrated in the experimental variogram. This is the reason why we will use this initial model for the further steps of estimation and conditional simulations.
@@ -58,17 +58,17 @@ err = kriging(dbin=mypoints, dbout=mygrid, model=mymodel, neigh=myneigh)
 mygrid$display()
 
 ########################
-p1 = plot.init(asp=1)
-p1 = p1 + plot.raster(mygrid, name="Kriging.Data.estim", flagLegend=TRUE, legendName="Estim", limits=c(-3.5, 3.5))
-p1 = p1 + plot.symbol(mypoints, nameSize="Data", color="yellow", sizeRange=c(0.1, 2))
-p1 = p1 + plot.decoration(title="Estimation", xlab="Easting", ylab="Northing")
-p2 = plot.init(asp=1)
-p2 = p2 + plot.raster(mygrid, name="Kriging.Data.stdev", palette="Spectral", flagLegend=TRUE, legendName="St.Dev.")
-p2 = p2 + plot.symbol(mypoints, nameSize="Data", flagCst=TRUE, color="black")
-p2 = p2 + plot.decoration(title="St. Dev. of Estimation Error", xlab="Easting", ylab="")
-p = ggarrange(p1, p2, ncol = 2, nrow = 1)
-plot.end(p)
-ggsave("Figure3_r.png", p, dpi = 600)
+#p1 = plot.init(asp=1)
+#p1 = p1 + plot.raster(mygrid, name="Kriging.Data.estim", flagLegend=TRUE, legendName="Estim", limits=c(-3.5, 3.5))
+#p1 = p1 + plot.symbol(mypoints, nameSize="Data", color="yellow", sizeRange=c(0.1, 2))
+#p1 = p1 + plot.decoration(title="Estimation", xlab="Easting", ylab="Northing")
+#p2 = plot.init(asp=1)
+#p2 = p2 + plot.raster(mygrid, name="Kriging.Data.stdev", palette="Spectral", flagLegend=TRUE, legendName="St.Dev.")
+#p2 = p2 + plot.symbol(mypoints, nameSize="Data", flagCst=TRUE, color="black")
+#p2 = p2 + plot.decoration(title="St. Dev. of Estimation Error", xlab="Easting", ylab="")
+#p = ggarrange(p1, p2, ncol = 2, nrow = 1)
+#plot.end(p)
+#ggsave("Figure3_r.png", p, dpi = 600)
 ########################
 
 # We can now construct 2 conditional simulations, using the Turning Bands algorithm again, but conditioned by the variable informed at the samples of mypoints. The newly created variables in mygrid are called *Simu.Data.1" and "Simu.Data.2". Each simulation outcome reproduces the spatial characteristics provided by the Model and honors the information provided at sample points.
@@ -77,15 +77,15 @@ err = simtub(dbin = mypoints, dbout = mygrid, model = mymodel, neigh = myneigh, 
 mygrid$display()
 
 ########################
-p1 = plot.init(asp=1)
-p1 = p1 + plot.raster(mygrid, name="Simu.Data.1", limits=c(-3.5, 3.5))
-p1 = p1 + plot.symbol(mypoints, nameSize="Data", color="yellow", sizeRange=c(0.1, 2))
-p1 = p1 + plot.decoration(title="Simulation 1", xlab="Easting", ylab="Northing")
-p2 = plot.init(asp=1)
-p2 = p2 + plot.raster(mygrid, name="Simu.Data.2", flagLegend=TRUE, legendName="Simu2", limits=c(-3.5, 3.5))
-p2 = p2 + plot.symbol(mypoints, nameSize="Data", color="yellow", sizeRange=c(0.1, 2))
-p2 = p2 + plot.decoration(title="Simulation 2", xlab="Easting", ylab="")
-p = ggarrange(p1, p2, ncol = 2, nrow = 1)
-plot.end(p)
-ggsave("Figure4_r.png", p, dpi=600)
+#p1 = plot.init(asp=1)
+#p1 = p1 + plot.raster(mygrid, name="Simu.Data.1", limits=c(-3.5, 3.5))
+#p1 = p1 + plot.symbol(mypoints, nameSize="Data", color="yellow", sizeRange=c(0.1, 2))
+#p1 = p1 + plot.decoration(title="Simulation 1", xlab="Easting", ylab="Northing")
+#p2 = plot.init(asp=1)
+#p2 = p2 + plot.raster(mygrid, name="Simu.Data.2", flagLegend=TRUE, legendName="Simu2", limits=c(-3.5, 3.5))
+#p2 = p2 + plot.symbol(mypoints, nameSize="Data", color="yellow", sizeRange=c(0.1, 2))
+#p2 = p2 + plot.decoration(title="Simulation 2", xlab="Easting", ylab="")
+#p = ggarrange(p1, p2, ncol = 2, nrow = 1)
+#plot.end(p)
+#ggsave("Figure4_r.png", p, dpi=600)
 ########################
