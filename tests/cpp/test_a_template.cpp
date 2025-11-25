26338d4a233142e08a270bf67686764fb8fe066c
drenard77810 <didier.renard@minesparis.psl.eu>
2025-11-25 15:40:55 +0100

    Modif

--- tests/cpp/test_a_template.cpp
+++ tests/cpp/test_a_template.cpp
@@ -28,41 +28,5 @@ int main(int argc, char* argv[])
   ASerializable::setPrefixName("test_a_template-"); // Here set the test name
   //
 
-  gstlrn::DbGrid* db_out = gstlrn::DbGrid::create({5, 5});
-  db_out->addColumns(
-    {
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      0.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-      1.,
-    },
-    "SEL", gstlrn::ELoc::SEL);
-  gstlrn::MeshETurbo* mesh = gstlrn::MeshETurbo::createFromGrid(db_out);
-
-  gstlrn::ProjMatrix* proj2D = gstlrn::ProjMatrix::create(db_out, mesh, -1, false);
-  OptCst::define(ECst::NTCOL, -1);
-  OptCst::define(ECst::NTROW, -1);
-  proj2D->display();
   return 0;
 }
