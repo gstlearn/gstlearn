0623d832b59d3eb8bc239c21eccb308817f1b8ed
drenard77810 <didier.renard@minesparis.psl.eu>
2026-01-15 20:18:52 +0100

    MAJ de Mo-Option.py

--- doc/UI/python/Mo-Option.py
+++ doc/UI/python/Mo-Option.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.11.25"
+__generated_with = "0.19.2"
 app = marimo.App()
 
 
@@ -15,8 +15,7 @@ def _():
 
     import numpy as np
     import pandas as pd
-
-    return ctx, gl, gmo, gp, mo, np, pd, plt
+    return ctx, gl, gmo, gp, mo
 
 
 @app.cell(hide_code=True)
@@ -28,6 +27,7 @@ def _():
 
 @app.cell(hide_code=True)
 def _(gmo):
+    # Définition du super-widget
     WidgetDb = gmo.WdefineDb()
     return (WidgetDb,)
 
@@ -40,8 +40,8 @@ def _(WidgetDb, gmo):
 
 @app.cell(hide_code=True)
 def _(db, gmo):
-    WidgetZoom = gmo.WdefineBox(db)
-    return (WidgetZoom,)
+    WidgetView = gmo.WdefineBox(db)
+    return (WidgetView,)
 
 
 @app.cell(hide_code=True)
@@ -51,21 +51,39 @@ def _(gmo, nxdef):
 
 
 @app.cell(hide_code=True)
-def _(gmo):
-    WidgetVario = gmo.WdefineVario(nlag=10, dlag=0.005)
+def _(db, gmo):
+    WidgetVario = gmo.WdefineVario(nlag=10, db=db)
     return (WidgetVario,)
 
 
 @app.cell(hide_code=True)
-def _(gmo):
-    WidgetCovList = gmo.WdefineCovList()
-    return (WidgetCovList,)
+def _(WidgetVario, db, gmo):
+    vario = gmo.WgetVario(WidgetVario, db=db)
+    return (vario,)
 
 
 @app.cell(hide_code=True)
-def _(WidgetCovList, WidgetDb, WidgetGrid, WidgetVario, ctx, gl, gmo, gp, mo):
+def _(gmo, vario):
+    WidgetModel = gmo.WdefineModel(ncovmax=3, vario=vario)
+    return (WidgetModel,)
+
+
+@app.cell(hide_code=True)
+def _(
+    WidgetDb,
+    WidgetGrid,
+    WidgetModel,
+    WidgetVario,
+    WidgetView,
+    ctx,
+    gl,
+    gmo,
+    gp,
+    mo,
+):
     def plotVario(ax, vario, model, showPairs=True):
         ax.varmod(vario, model, showPairs=showPairs)
+        ax.decoration(title="Variogram & Model")
 
     def plotData(ax, db, box, targetName, flagProj=False):
         ax.baseMap(db=db, box=box, flagProj=flagProj)
@@ -74,7 +92,8 @@ def _(WidgetCovList, WidgetDb, WidgetGrid, WidgetVario, ctx, gl, gmo, gp, mo):
             ctx.add_basemap(
                 ax, source=ctx.providers.OpenStreetMap.Mapnik, crs="EPSG:4326"
             )
-        ax.decoration(title=targetName + " (long/lat)")
+        ax.decoration(title=targetName)
+        ax.geometry(aspect=1)
 
     def plotEstim(ax, db, grid, targetName, flagProj=False):
         ax.raster(dbgrid=grid, name="Kriging.*.estim", alpha=0.5)
@@ -84,6 +103,7 @@ def _(WidgetCovList, WidgetDb, WidgetGrid, WidgetVario, ctx, gl, gmo, gp, mo):
                 ax, source=ctx.providers.OpenStreetMap.Mapnik, crs="EPSG:4326"
             )
         ax.decoration(title="Estimation")
+        ax.geometry(aspect=1)
 
     def plotStdev(ax, db, grid, targetName, flagProj=False):
         ax.raster(dbgrid=grid, name="Kriging.*.stdev", alpha=0.5)
@@ -93,6 +113,7 @@ def _(WidgetCovList, WidgetDb, WidgetGrid, WidgetVario, ctx, gl, gmo, gp, mo):
                 ax, source=ctx.providers.OpenStreetMap.Mapnik, crs="EPSG:4326"
             )
         ax.decoration(title="St. Dev. of Estimation Error")
+        ax.geometry(aspect=1)
 
     def myaction():
         # Define the Input Db
@@ -100,25 +121,24 @@ def _(WidgetCovList, WidgetDb, WidgetGrid, WidgetVario, ctx, gl, gmo, gp, mo):
         if db is None:
             return None
 
-        # Check that the Db is 2-D and contains the variable of interest
-        targetName = "pH"
-        db.setLocators(["longitude", "latitude"], gl.ELoc.X)
-        db.setLocator("sel", gl.ELoc.SEL)  # Set the selection (if present in CSV)
-        db.setLocator(targetName, gl.ELoc.Z)
+        targetName = db.getNameByLocator(gl.ELoc.Z)
         ndim = db.getNLoc(gl.ELoc.X)
-        if ndim != 2 or db.getColIdx(targetName) < 0:
-            print("The 'db' should be 2-D (", ndim, ") and contain the target variable")
+        if ndim != 2:
+            print("The 'db' should be 2-D (", ndim, ")")
+            return None
+        if db.getColIdx(targetName) < 0:
+            print("The 'db' should contain the target variable")
             return None
 
         # Define the output Grid
-        box = db.getExtremas()
+        box, flagProj = gmo.WgetBox(WidgetView)
         grid = gmo.WgetGridN(WidgetGrid, box)
 
-        # Define the Variogram parameters
+         # Define the Variogram parameters
         vario = gmo.WgetVario(WidgetVario, db)
 
         # Define the Model
-        model = gmo.WgetCovList(WidgetCovList, vario)
+        model = gmo.WgetModel(WidgetModel, vario)
 
         # Define Neighborhood (Unique)
         neigh = gl.NeighUnique.create()
@@ -127,7 +147,7 @@ def _(WidgetCovList, WidgetDb, WidgetGrid, WidgetVario, ctx, gl, gmo, gp, mo):
         err = gl.kriging(db, grid, model, neigh)
 
         fig, ax = gp.init(2, 2, figsize=(10, 10))
-        plotData(ax[0, 0], db, box, targetName)
+        plotData(ax[0, 0], db, box, targetName, flagProj=flagProj)
         plotVario(ax[0, 1], vario, model, showPairs=True)
 
         plotEstim(ax[1, 0], db, grid, targetName)
@@ -135,17 +155,16 @@ def _(WidgetCovList, WidgetDb, WidgetGrid, WidgetVario, ctx, gl, gmo, gp, mo):
         mo.mpl.interactive(fig)
 
         return fig
-
-    return myaction, plotData, plotEstim, plotStdev, plotVario
+    return (myaction,)
 
 
 @app.cell(hide_code=True)
 def _(
-    WidgetCovList,
     WidgetDb,
     WidgetGrid,
+    WidgetModel,
     WidgetVario,
-    WidgetZoom,
+    WidgetView,
     gmo,
     mo,
     myaction,
@@ -153,19 +172,19 @@ def _(
     param = mo.ui.tabs(
         {
             "Data": gmo.WshowDb(WidgetDb),
-            "Zoom": gmo.WshowBox(WidgetZoom),
+            "View": gmo.WshowBox(WidgetView, gapv=0, gaph=1),
             "Grid": gmo.WshowGridN(WidgetGrid),
             "Variogram": gmo.WshowVario(WidgetVario),
-            "Model": gmo.WshowCovList(WidgetCovList),
+            "Model": gmo.WshowModel(WidgetModel),
         }
     ).style({"minWidth": "350px", "width": "350px"})
 
     simu = mo.vstack(
-        [mo.md(""), mo.md(f"Data and its Estimation{mo.as_html(myaction())}")], gap=4
+        [mo.md(""), mo.md(f"Data and its Estimation {mo.as_html(myaction())}")], gap=4
     )
 
     mo.hstack([param, simu], gap=4)
-    return param, simu
+    return
 
 
 if __name__ == "__main__":
