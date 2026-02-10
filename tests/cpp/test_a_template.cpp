04a494cda3a2b21b451afcb6225de50143ee505f
drenard77810 <didier.renard@minesparis.psl.eu>
2026-02-10 19:38:24 +0100

    Relancer la PR

--- tests/cpp/test_a_template.cpp
+++ tests/cpp/test_a_template.cpp
@@ -9,10 +9,6 @@
 /*                                                                            */
 /******************************************************************************/
 #include "Basic/ASerializable.hpp"
-#include "Covariances/CorGneiting.hpp"
-#include "Db/DbGrid.hpp"
-#include "Model/ModelGeneric.hpp"
-#include "Simulation/Simulations.hpp"
 #include "geoslib_define.h"
 
 using namespace gstlrn;
@@ -24,27 +20,6 @@ int main(int argc, char* argv[])
   sfn << gslBaseName(__FILE__) << ".out";
   StdoutRedirect sr(sfn.str(), argc, argv);
   ASerializable::setPrefixName("test_a_template-"); // Here set the test name
-
-  auto ctxt = CovContext(1, 1);
-
-  VectorDouble scales  = {1};
-  double alpha         = 1.5;
-  double beta          = 1;
-  double nu            = 3. / 4.;
-  double timerange     = 2.5;
-  auto* mg_in_gstlearn = CorGneiting::create(ctxt, ECov::GNEITING_G, alpha, beta, timerange,
-                                             {nu}, {1.0}, scales, VectorDouble(), 1.0, false);
-  auto modelGeneric    = ModelGeneric(ctxt);
-  (void)modelGeneric.setCov(mg_in_gstlearn);
-
-  VectorInt nx    = {100, 50};
-  VectorDouble dx = {0.15, 0.3};
-  auto* grd       = DbGrid::create(nx, dx);
-
-  Id nsim = 20;
-  Id ns   = 5000;
-  (void)simuSpectral(nullptr, grd, &modelGeneric, nullptr, nsim, 0, ns, 100, nullptr, true,
-                     NamingConvention("test_a_template"));
 
   return 0;
 }
