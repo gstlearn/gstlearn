8e3f71dbd9bb056556a4088f3895b98f8ec1a70a
drenard77810 <didier.renard@minesparis.psl.eu>
2026-02-11 12:59:58 +0100

    cleaning Aanam

--- src/Anamorphosis/CalcAnamTransform.cpp
+++ src/Anamorphosis/CalcAnamTransform.cpp
@@ -56,16 +56,16 @@ CalcAnamTransform::~CalcAnamTransform()
 {
 }
 
-bool CalcAnamTransform::_hasAnam(const EAnam& anamType) const
+bool CalcAnamTransform::_hasAnam(bool flagAny, const EAnam& anamType) const
 {
   if (_anam == nullptr)
   {
     messerr("The argument 'anam' must be defined");
     return false;
   }
-  if (anamType != _anam->getType())
+  if (!flagAny && anamType != _anam->getType())
   {
-    messerr("The argument 'anam'  should be of type");
+    messerr("The argument 'anam' should be of type %s", anamType.getKey().data());
     return false;
   }
   return true;
@@ -187,7 +187,7 @@ bool CalcAnamTransform::_check()
 
   if (_flagDisjKrig)
   {
-    if (!_hasAnam(EAnam::HERMITIAN)) return false;
+    if (!_hasAnam(false, EAnam::HERMITIAN)) return false;
     if (!_hasInputVarDefined()) return false;
     if (!_hasSelectivity()) return false;
     return true;
@@ -195,7 +195,7 @@ bool CalcAnamTransform::_check()
 
   if (_flagCondExp)
   {
-    if (!_hasAnam(EAnam::HERMITIAN)) return false;
+    if (!_hasAnam(false, EAnam::HERMITIAN)) return false;
     if (!_hasInputVarDefined()) return false;
     if (!_hasSelectivity()) return false;
     return true;
@@ -203,7 +203,7 @@ bool CalcAnamTransform::_check()
 
   if (_flagUniCond)
   {
-    if (!_hasAnam(EAnam::HERMITIAN)) return false;
+    if (!_hasAnam(false, EAnam::HERMITIAN)) return false;
     if (!_hasInputVarDefined(1)) return false;
     if (!_hasSelectivity()) return false;
     if (_selectivity->isUsed(ESelectivity::Z))
