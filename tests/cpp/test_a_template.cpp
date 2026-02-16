2b5ec4f9514fa494da486bddef6b5394b0a508b0
drenard77810 <didier.renard@minesparis.psl.eu>
2026-02-16 14:10:37 +0100

    Modif de test_a_template

--- tests/cpp/test_a_template.cpp
+++ tests/cpp/test_a_template.cpp
@@ -9,10 +9,7 @@
 /*                                                                            */
 /******************************************************************************/
 #include "Basic/ASerializable.hpp"
-#include "Db/Db.hpp"
-#include "Model/Model.hpp"
 #include "geoslib_define.h"
-#include "geoslib_f.h"
 
 using namespace gstlrn;
 
@@ -23,44 +20,6 @@ int main(int argc, char* argv[])
   sfn << gslBaseName(__FILE__) << ".out";
   StdoutRedirect sr(sfn.str(), argc, argv);
   ASerializable::setPrefixName("test_a_template-"); // Here set the test name
-
-  auto* data = Db::createFromNF("/home/drenard/DataBefore.NF");
-  data->deleteColumn("g*");
-  data->deleteColumn("es.y.*");
-  data->deleteColumn("*Bound");
-  data->deleteColumn("code");
-  data->deleteColumn("Simu");
-  data->deleteColumn("Ind");
-
-  auto* model = Model::createFromParam(ECov::EXPONENTIAL, 0.5);
-  model->display();
-
-  auto nbsimu          = 4;
-  auto nburn           = 10;
-  auto niter           = 30;
-  auto gibbsOptStats   = 0;
-  auto percent         = 0.;
-  auto flagMoving      = true;
-  auto flagNorm        = false;
-  auto flagMultiMono   = false;
-  auto flagPropagation = false;
-  auto flagSymNeigh    = false;
-  auto flagCE          = false;
-  auto flagSTD         = false;
-  auto verbose         = false;
-  VectorInt seed_gb    = {2113, 7555, 6002, 4124};
-
-  data->display();
-  for (Id s = 0; s < nbsimu; s++)
-  {
-    message(">>>> processing simulation #%d\n", s + 1);
-    std::string s_nm = "cs.y." + std::to_string(s);
-    data->clearLocators(ELoc::Z);
-    (void)gibbs_sampler(data, model, 1, seed_gb[s], nburn, niter, flagMoving, flagNorm, flagMultiMono,
-                        flagPropagation, flagSymNeigh, gibbsOptStats, percent, flagCE, flagSTD, verbose,
-                        NamingConvention(s_nm));
-  }
-  data->getStatsAsTable({"cs.y.*"}).display();
 
   return 0;
 }
