12ff9a23a5cc669c2e6628c72ef56ad27feb7aac
drenard77810 <didier.renard@minesparis.psl.eu>
2026-04-03 15:42:39 +0200

    Upgrade the non-regression tests before big reformatting

--- tests/cpp/test_a_template.cpp
+++ tests/cpp/test_a_template.cpp
@@ -9,11 +9,6 @@
 /*                                                                            */
 /******************************************************************************/
 #include "Basic/ASerializable.hpp"
-#include "Matrix/AMatrix.hpp"
-#include "Matrix/MatrixDense.hpp"
-#include "Matrix/MatrixFactory.hpp"
-#include "Matrix/MatrixSquare.hpp"
-#include "Matrix/MatrixSymmetric.hpp"
 #include "geoslib_define.h"
 
 using namespace gstlrn;
@@ -25,11 +20,6 @@ int main(int argc, char* argv[])
   sfn << gslBaseName(__FILE__) << ".out";
   StdoutRedirect sr(sfn.str(), argc, argv);
   ASerializable::setPrefixName("test_a_template-"); // Here set the test name
-
-  auto M1    = MatrixSquare(7, 7);
-  auto M2    = MatrixSymmetric(7, 7);
-  AMatrix* M = MatrixFactory::prodMatMat(&M1, &M2);
-  M->printConcreteClassName();
 
   return 0;
 }
