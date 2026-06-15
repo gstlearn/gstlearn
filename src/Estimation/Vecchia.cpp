117ad4f2fe476ebda939f5bfee40baf9b0b4a83e
drenard77810 <didier.renard@minesparis.psl.eu>
2026-06-15 15:45:23 +0200

    Correction of Naming convention

--- src/Estimation/Vecchia.cpp
+++ src/Estimation/Vecchia.cpp
@@ -689,7 +689,8 @@ namespace gstlrn
 
     // Saving the results
     Id iptr = dbout->addColumns(result, String(), ELoc::UNDEFINED, 0, true);
-    namconv.setNamesAndLocators(dbout, iptr, "estim", nvar);
+    namconv.setNamesAndLocators(
+      dbin, VectorString(), ELoc::Z, nvar, dbout, iptr, "estim");
 
     return 0;
   }
