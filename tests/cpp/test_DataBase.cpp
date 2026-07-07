a889326d7273e7b3f7a80e6d46ff40c514446a35
drenard77810 <didier.renard@minesparis.psl.eu>
2026-07-07 16:07:08 +0200

    Avec tests de nonreg

--- tests/cpp/test_DataBase.cpp
+++ tests/cpp/test_DataBase.cpp
@@ -8,6 +8,7 @@
 /* License: BSD 3-clause                                                      */
 /*                                                                            */
 /******************************************************************************/
+#include "Basic/ASerializable.hpp"
 #include "Basic/VectorNumT.hpp"
 #include "DataBase/DbData.hpp"
 
@@ -20,23 +21,32 @@ using namespace gstlrn;
  ** This program is meant to check the manipulation of the Db
  **
  *****************************************************************************/
-int main()
+int main(int argc, char* argv[])
 {
+  std::stringstream sfn;
+  sfn << gslBaseName(__FILE__) << ".out";
+  StdoutRedirect sr(sfn.str(), argc, argv);
+
+  ASerializable::setPrefixName("test_DataBase-");
+
   DbData data{};
 
   data.addArray(VectorDouble{1., 2., 3.}, "hello");
   data.addArray(VectorInt{5, 6, 7}, "world");
   data.addArray(VectorString{"foo", "bar", "baz"}, "foobar");
-  std::cout << data.getValue<double>(2, 0).value_or(-1.) << " "
-            << data.getValue<int>(2, 1).value_or(-1) << " "
-            << data.getValue<String>(2, 2).value_or("failed") << "\n";
+
+  message("Initial values\n");
+  message("%lf\n", data.getValue<double>(2, 0).value_or(-1.));
+  message("%d\n", data.getValue<int>(2, 1).value_or(-1));
+  message("%s\n", data.getValue<String>(2, 2).value_or("failed").c_str());
 
   data.setValue(4., 2, 0);
   data.setValue(8, 2, 1);
   data.setValue("foobar", 2, 2);
 
-  std::cout << data.getValue<double>(2, 0).value_or(-1.) << " "
-            << data.getValue<int>(2, 1).value_or(-1) << " "
-            << data.getValue<String>(2, 2).value_or("failed") << "\n";
+  message("Values after modification\n");
+  message("%lf\n", data.getValue<double>(2, 0).value_or(-1.));
+  message("%d\n", data.getValue<int>(2, 1).value_or(-1));
+  message("%s\n", data.getValue<String>(2, 2).value_or("failed").c_str());
   return 0;
 }
